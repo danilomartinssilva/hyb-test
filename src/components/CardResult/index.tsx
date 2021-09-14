4c44da7e31207903f1335480c3c673ba67f237d7
Danilo <danilomartins.pacs@gmail.com>
2021-09-14 12:03:17 -0300

    feat: implement CardResult

---
+++ src/components/CardResult/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Container ,ContainerIconExtract,ContainerInfo,ContainerInfoSalary,ContainerTitle, TextDescriptionResume, TextDescriptionValue, Title,ContainerInfoVisibility} from './styles'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import * as colors from '../../assets/theme/colors'
+import formatMonetary from '../../utils/formatMonerary';
+
+
+// import { Container } from './styles';
+
+interface CardResultProps{
+  value:number;
+
+}
+
+function  CardResult ({value}:CardResultProps)  {
+  
+  return <>
+         <Container>
+           <ContainerTitle>
+             <Title>
+               Conta Pessoa Física
+             </Title>
+             <ContainerIconExtract>
+                  <MaterialCommunityIcons 
+                  name = {'arrow-left-right'}
+                  color = {colors.white}
+                  size={20}
+                  />
+             </ContainerIconExtract>
+             
+           </ContainerTitle>
+
+          <ContainerInfo>
+            <ContainerInfoSalary>
+              <TextDescriptionResume>Saldo disponível</TextDescriptionResume>
+              <TextDescriptionResume>R$ {' '} <TextDescriptionValue>{formatMonetary(value)}</TextDescriptionValue></TextDescriptionResume>              
+            </ContainerInfoSalary> 
+            <ContainerInfoVisibility>
+              <MaterialCommunityIcons 
+              name = "eye"
+              size = {20}
+              color = {colors.white}
+              
+              />
+            </ContainerInfoVisibility>
+          </ContainerInfo>
+
+
+
+         </Container>
+         </>
+}
+
+export default CardResult;
