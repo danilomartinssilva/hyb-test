7fdd824547931c44ee001367427fef5ee0de04d1
Danilo <danilomartins.pacs@gmail.com>
2021-09-14 12:03:01 -0300

    feat: implement Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { AvatarCircle, BioContainer, ButtonConfiguration, Container, UserInfoContainer, UserInfoText } from './styles';
+import * as colors from '../../assets/theme/colors'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+// import { Container } from './styles';
+
+const Header: React.FC = () => {
+    return (
+        <Container>
+          <BioContainer>
+           <UserInfoContainer>
+            <AvatarCircle source = {{uri:"https://i.pravatar.cc/40"}} />
+
+            <UserInfoText> Danilo Martins</UserInfoText>
+           </UserInfoContainer>
+           <ButtonConfiguration>
+             <Ionicons
+                name="settings-outline"
+                size = {25}
+                color = {colors.white}
+              />
+             
+           </ButtonConfiguration>
+          
+          </BioContainer>
+        </Container>
+    ) 
+
+}
+
+export default Header;
