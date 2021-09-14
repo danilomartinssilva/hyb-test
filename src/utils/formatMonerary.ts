4fff865d21fb9a2b6479d5a866adc570acf5b03f
Danilo <danilomartins.pacs@gmail.com>
2021-09-14 12:01:15 -0300

    feat: implement formatMonetary

---
+++ src/utils/formatMonerary.ts
@@ -0,0 +1,7 @@
+function formatMonetary(value:number){
+  return value.toFixed(2) // casas decimais
+  .replace('.', ',')
+  .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+}
+
+export default formatMonetary
